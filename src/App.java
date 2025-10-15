a75d0f9039fcbdf7e4803b39f4f2aa9b54586006
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-15 21:10:34 +0200

    First commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int num = Integer.parseInt(System.console().readLine("Por favor, introduzca un número entero (5 cifras como máximo): "));
+
+        if ((num <= 0) && (num > -10)) {
+            System.out.println("El número introducido tiene 1 dígitos.");
+        } else if ((num <= -10) && (num > -100)) {
+            System.out.println("El número introducido tiene 2 dígitos.");
+        } else if ((num <= -100) && (num > -1000)) {
+            System.out.println("El número introducido tiene 3 dígitos.");
+        } else if ((num <= -1000) && (num > -10000)) {
+            System.out.println("El número introducido tiene 4 dígitos.");
+        } else if ((num <= -10000) && (num > -100000)) {
+            System.out.println("El número introducido tiene 5 dígitos.");
+        } else if ((num >= 0) && (num < 10)){
+            System.out.println("El número introducido tiene 1 dígitos.");
+        } else if ((num >= 10) && (num < 100)) {
+            System.out.println("El número introducido tiene 2 dígitos.");
+        } else if ((num >= 100) && (num < 1000)) {
+            System.out.println("El número introducido tiene 3 dígitos.");
+        } else if ((num >= 1000) && (num < 10000)) {
+            System.out.println("El número introducido tiene 4 dígitos.");
+        } else if ((num >= 10000) && (num < 100000)) {
+            System.out.println("El número introducido tiene 5 dígitos.");
+        }
     }
 }
